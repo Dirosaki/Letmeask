ffb31ed4a8aeaf937de9d4df16c34ad1facd27a3
Dirosaki <diegosgomes27@gmail.com>
2021-06-25 01:39:35 -0300

    Adição de questions vazias na pagina de admin

--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -10,6 +10,7 @@ import { Question } from "../../components/Question";
 import { database } from "../../services/firebase";
 
 import logoImg from "../../assets/images/logo.svg";
+import emptyQuestionImg from "../../assets/images/empty-questions.svg";
 
 import "./style.scss";
 
@@ -34,6 +35,18 @@ export function AdminRoom() {
 
       history.push("/");
     }
+  }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true,
+    });
   }
 
   async function handleDeleteQuestion(questionId: string) {
@@ -61,6 +74,16 @@ export function AdminRoom() {
           <h1>Sala {title}</h1>
           {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
         </div>
+        {questions.length === 0 && (
+          <div className="empty-questions">
+            <img src={emptyQuestionImg} alt="" />
+            <strong>Nenhuma pergunta por aqui...</strong>
+            <p>
+              Envie o código desta sala para seus amigos e<br /> comece a
+              responder perguntas!
+            </p>
+          </div>
+        )}
 
         <div className="question-list">
           {questions.map((question) => (
@@ -68,10 +91,67 @@ export function AdminRoom() {
               content={question.content}
               author={question.author}
               key={question.id}
+              isAnswered={question.isAnswered}
+              isHighlighted={question.isHighlighted}
             >
+              {!question.isAnswered && (
+                <>
+                  <button
+                    type="button"
+                    onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                  >
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle
+                        cx="12.0003"
+                        cy="11.9998"
+                        r="9.00375"
+                        stroke="#737380"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M8.44287 12.3391L10.6108 14.507L10.5968 14.493L15.4878 9.60193"
+                        stroke="#737380"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => handleHighlightQuestion(question.id)}
+                  >
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M12 17.9999H18C19.657 17.9999 21 16.6569 21 14.9999V6.99988C21 5.34288 19.657 3.99988 18 3.99988H6C4.343 3.99988 3 5.34288 3 6.99988V14.9999C3 16.6569 4.343 17.9999 6 17.9999H7.5V20.9999L12 17.9999Z"
+                        stroke="#737380"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </button>
+                </>
+              )}
               <button
                 type="button"
-                className="trash-question"
+                className="delete-question"
                 onClick={() => handleDeleteQuestion(question.id)}
               >
                 <svg
